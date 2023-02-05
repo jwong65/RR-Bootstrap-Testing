e73db753aad4af7987e06a3a11b0ceed7e4e0224
jwong65 <jason.wong6507@gmail.com>
2023-02-05 14:09:23 -0500

    Added the text that will be overlayed on card

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -3,10 +3,11 @@ import Card from 'react-bootstrap/Card'
 
 export default function BackgroundImage() {
   return (
-    <Card>
-        <Card.Img src='https://d2d8wwwkmhfcva.cloudfront.net/1500x1520/d2guulkeunn7d8.cloudfront.net/assets/homepage/homepage-mobile-background-m3-high-res-ca3279087ea0a3337f671b694b196d43fd207443d5825e61295b0996974835e9.jpg' alt='background' />
-        <Card.ImgOverlay>
+    <Card className='bg-dark text-white'>
+        <Card.Img style={{position: 'absolute'}}src='https://d2d8wwwkmhfcva.cloudfront.net/1500x1520/d2guulkeunn7d8.cloudfront.net/assets/homepage/homepage-mobile-background-m3-high-res-ca3279087ea0a3337f671b694b196d43fd207443d5825e61295b0996974835e9.jpg' alt='background' />
+        <Card.ImgOverlay style={{position: 'relative', left: '5em', color: 'white', fontWeight: 'bold'}}>
             <Card.Title>Order Groceries?</Card.Title>
+            <Card.Text>Groceries have to be ordered.</Card.Text>
         </Card.ImgOverlay>
     </Card>
   )
