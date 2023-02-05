deb55f53de8069e70ecfbf369a3216d640a75a80
jwong65 <jason.wong6507@gmail.com>
2023-02-05 13:14:50 -0500

    Moved the component configuration into another file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
 import './App.css';
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Container from 'react-bootstrap/Container';
+import TopBar from './components/TopBar';
 
 function App() {
   return (
-    <Navbar bg='light' expand='lg'>
-      <Container>
-        <Navbar.Brand style={{margin: '10px'}}>
-          This is a Brand.
-        </Navbar.Brand>
-      <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-        <Navbar.Collapse id ='basic-navbar-nav'>
-          <Nav className='me-auto'>
-            <Nav.Link href='#LogTest'>Log In Test</Nav.Link>
-            <NavDropdown id='basic-nav-dropdown' title='DropDown Testing'>
-              <NavDropdown.Item href=''>Action</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item>Another item</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <TopBar />
   );
 }
 
---
+++ src/components/TopBar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
+import { Button } from 'react-bootstrap';
+
+export default function TopBar() {
+  return (
+    <Navbar expand='lg' bg='light'>
+      <Container>
+      <Navbar.Brand style={{margin: '10px 0 0 20px'}}>
+        This is a Brand.
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
+      <Navbar.Collapse id ='basic-navbar-nav'>
+        <Navbar.Text>
+        <Nav className='me-auto'>
+          <Nav.Link href='#LogTest'>Log In Test</Nav.Link>
+          <Button variant ='success'>Sign up Test</Button>
+          <NavDropdown id='basic-nav-dropdown' title='DropDown Testing'>
+            <NavDropdown.Item href=''>Action</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item>Another item</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        </Navbar.Text>
+      </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
