eb1c3bcdd4ed0dd014d1bd8b660ba0c3f114b01a
jwong65 <jason.wong6507@gmail.com>
2023-02-05 12:45:22 -0500

    Changed the Container so it is imported from react-bootstrap

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import './App.css';
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <Navbar bg='light' expand='lg'>
-      <Navbar.Brand style={{margin: '10px'}}>
-        This is a Brand.
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
-      <Navbar.Collapse id ='basic-navbar-nav'>
-        <Nav>
-          <Nav.Link href='#LogTest'>Log In Test</Nav.Link>
-          <NavDropdown id='basic-nav-dropdown' title='DropDown Testing'>
-            <NavDropdown.Item href=''>Action</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item>Another item</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
+      <Container>
+        <Navbar.Brand style={{margin: '10px'}}>
+          This is a Brand.
+        </Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+        <Navbar.Collapse id ='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link href='#LogTest'>Log In Test</Nav.Link>
+            <NavDropdown id='basic-nav-dropdown' title='DropDown Testing'>
+              <NavDropdown.Item href=''>Action</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item>Another item</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
