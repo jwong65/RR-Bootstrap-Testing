a09e4b63537c361a7779c526c47471a4a0bf8bc7
jwong65 <jason.wong6507@gmail.com>
2023-02-05 12:29:26 -0500

    Removed the components, and correctly imported the bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
-import NavBar from './components/NavBar';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
   return (
-    <NavBar bg='light' expand='lg'>
-    </NavBar>
+    <Navbar bg='light' expand='lg'>
+      <Navbar.Brand style={{margin: '10px'}}>
+        This is a Brand.
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
+      <Navbar.Collapse id ='basic-navbar-nav'>
+        <Nav className='test-auto'>
+        </Nav>
+        <Nav>
+          <Nav.Link href='#LogTest'>Log In Test</Nav.Link>
+          <Nav className='testDropdown'>
+            <NavDropdown.Item href=''>Action</NavDropdown.Item>
+          </Nav>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
