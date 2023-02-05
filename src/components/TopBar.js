95be41d3c70e8f3de97592917b5df898534aa28c
jwong65 <jason.wong6507@gmail.com>
2023-02-05 13:30:25 -0500

    Removed the NavDropdown after testing

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,29 +2,24 @@ import React from 'react'
 
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import { Button } from 'react-bootstrap';
 
 export default function TopBar() {
   return (
-    <Navbar expand='lg' bg='light'>
+    <Navbar expand='lg' bg='primary' variant='dark'>
       <Container>
-      <Navbar.Brand style={{margin: '10px 0 0 20px'}}>
-        This is a Brand.
-      </Navbar.Brand>
+        <Navbar.Brand style={{margin: '10px 0 0 20px'}}>
+            This is a Brand.
+        </Navbar.Brand>
+      </Container>
+      
       <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
-      <Navbar.Collapse id ='basic-navbar-nav' style={{display: 'flex', margin: '1em 0 0 1em'}}>
+      <Navbar.Collapse id ='basic-navbar-nav' style={{display: 'flex-container', padding:"1em 10px 10px 1em"}}>
         <Nav className='mr-auto'></Nav>
           <Nav.Link href='#LogTest'>Log In Test</Nav.Link>
           <Button variant ='success'>Sign up Test</Button>
-          <NavDropdown id='basic-nav-dropdown' title='DropDown Testing'>
-            <NavDropdown.Item href=''>Action</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item>Another item</NavDropdown.Item>
-          </NavDropdown>
       </Navbar.Collapse>
-      </Container>
     </Navbar>
   )
 }
