ffb259a2f197770bd4e2941fa7a01e1582c15e97
jwong65 <jason.wong6507@gmail.com>
2023-02-07 18:03:02 -0500

    Added the StoreListing and added skeleton with Container/Row/Col

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import TopBar from './components/TopBar';
 import BackgroundImage from './components/BackgroundImage';
+import StoreListing from './components/StoreListing';
 
 function App() {
   return (
     <>
       <TopBar />
       <BackgroundImage />
+      <StoreListing />
     </>
   );
 }
---
+++ src/components/StoreListing.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+export default function StoreListing() {
+  return (
+    <Container>
+        <Row>
+            <Col> </Col>
+            <Col> </Col>
+        </Row>
+        <Row>
+            <Col></Col>
+            <Col></Col>
+        </Row>
+    </Container>
+  )
+}
