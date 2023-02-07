6d23e32212797316e3795358e96acb6a6d2529dd
jwong65 <jason.wong6507@gmail.com>
2023-02-07 18:41:54 -0500

    Three rows rendered with fontWeight

--- src/components/StoreListing.js
+++ src/components/StoreListing.js
@@ -5,15 +5,21 @@ import Col from 'react-bootstrap/Col'
 
 export default function StoreListing() {
   return (
-    <Container fluid='lg' style={{position: 'relative', marginTop: '40px', textAlign: 'center'}}>
-        <Row style={{justifyContent:"space-evenly", display: 'flex'}}>
-            <Col sm={8}>One of 2 </Col>
-            <Col sm={4}>Two of 2 </Col>
+    <Container fluid='lg' style={{position: 'relative', marginTop: '40px', textAlign: 'center', top: '3em', fontWeight: 'bold', color: 'Red'}}>
+    <Row style={{justifyContent:"space-evenly", display: 'flex'}}>
+        <Row>
+            <Col sm={8}><p>One of 2</p> </Col>
+            <Col sm={4}><p>Two of 2</p> </Col>
         </Row>
         <Row>
-            <Col></Col>
-            <Col></Col>
+            <Col><p>Row 2, One of 3</p></Col>
+            <Col><p>Row 2, Two of 3</p></Col>
+            <Col><p>Row 2, Three of 3</p></Col>
         </Row>
+        <Row>
+            <Col><p>Row 3, One of 1</p></Col>
+        </Row>
+    </Row>
     </Container>
   )
 }
